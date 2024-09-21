9a7030b46bc563d50ab15299f54cfcd325b4dbb7
Nikki Yodo <nik_yodo@yahoo.com>
2024-09-20 21:32:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Penny Web Hosting in AWS
+
+A quick demo on how to setup a very simple website in AWS
---
+++ scripts/setup.ps1
@@ -0,0 +1,4 @@
+npx create-next-app@latest
+npm install react-icons --save
+
+aws cloudformation create-stack --stack-name awskcug-web --template-body ../infrastructure/aws/web.yml --region us-east-1
