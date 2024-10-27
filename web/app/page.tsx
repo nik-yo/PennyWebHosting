28f1134eaf7bfc5f57eb7631129237e03e8f4117
Nikki Yodo <nik_yodo@yahoo.com>
2024-10-27 08:54:06 -0500

    add simple web

---
+++ web/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div className="h-80 bg-[url('/awskcug_logo.png')] bg-no-repeat bg-contain bg-center"></div>
+        <p className="text-2xl text-center font-bold my-4">AWS Kansas City User Group</p>
+      </main>
+    </div>
+  );
+}
